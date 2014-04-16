9c28f49cd09e68e7983218d413600b6cec5f2b6f
Darshan Patel <dp392@cornell.edu>
2014-04-16 13:21:00 -0400

    added controllers for generate.html

--- angular-drippr/app/js/controllers.js
+++ angular-drippr/app/js/controllers.js
@@ -5,11 +5,20 @@
 var dripprControllers = angular.module('dripprControllers', []);
 
 dripprControllers.controller('generateCtrl', ['$scope', '$http',
-  function($scope, $http) {
-    $http.get('json/articles.json').success(function(data) {
-      $scope.articles = data;
-    });
-    // $scope.orderProp = 'age';
-  }]);
+	function($scope, $http) {
+		$http.get('json/articles.json').success(function(data) {
+			$scope.articles = data;
+   		});
 
+	var like = 0;
+   	$scope.like = function() {
+		like = like + 1;
+		console.log(like);  //link to database
+	};
 
+	var dislike = 0;
+	$scope.dislike = function() {
+		dislike = dislike + 1;
+		console.log(dislike);  //link to database
+	};
+}]);
