f102efdc8316b17ad61a4d8ce913b1f167793ae7
Darshan Patel <dp392@cornell.edu>
2014-04-21 16:41:41 -0400

    added images and rss

---
+++ fuck/rss.py
@@ -0,0 +1,30 @@
+import feedparser, urllib2, csv
+
+rss = ["http://feeds.foxnews.com/foxnews/latest",'http://sports.espn.go.com/espn/rss/news']
+rss2 = ['http://hosted2.ap.org/atom/APDEFAULT/3d281c11a96b4ad082fe88aa0db04305']
+
+
+
+with open('Articles.csv', 'wb') as f:
+	writer = csv.writer(f)
+	writer.writerow(["Title","URL","Image","Date","Source"])
+	for sources in rss:
+		feed = feedparser.parse(sources)
+		lst = feed['items']
+		source = feed[ "channel" ][ "title" ]
+		for a in lst:
+			title = a['title']
+			url = a['link']
+			date = a['published']
+			request = urllib2.Request(url)
+			request.add_header("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; es-ES; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5")
+			usock = urllib2.urlopen(request)
+			data = usock.read()
+			usock.close()
+			try:
+				beg = data.rindex('"',0,data.find('jpg')) + 1
+				end = data.index('"',data.find('jpg'))
+				image = data[beg:end]
+			except:
+				image = 'Stock Photo'
+			writer.writerow([title,url,image,date,source])
