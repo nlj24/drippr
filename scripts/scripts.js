4412d24d6b755a42bc5a9720efa750a7a2d98ce0
Darshan Patel <dp392@cornell.edu>
2014-04-22 01:05:59 -0400

    cleaned up repo

---
+++ scripts/scripts.js
@@ -0,0 +1,56 @@
+var ARTICLE_METHOD ={
+ 
+        handlerData:function(resJSON){
+ 
+            var templateSource   = $("#choose-template").html(),
+ 
+                template = Handlebars.compile(templateSource),
+ 
+                articleHTML = template(resJSON);
+ 
+           $('#my-container').html(articleHTML);
+           	$(".like").click(function(e){
+			// e.preventDefault();
+				console.log($(e.target).attr("article"));
+				//ajax request to add this like, if the user hasn't liked it before
+				resJSON.articles[0].numLikes++;
+				articleHTML = template(resJSON);
+ 
+           		$('#my-container').html(articleHTML);
+			});
+
+			$(".dislike").click(function(e){
+			// e.preventDefault();
+				console.log($(e.target).attr("article"));
+				//ajax request to add this like, if the user hasn't liked it before
+			});
+
+            $(".dripp").click(function(e){
+            // e.preventDefault();
+                alert("add ability to send");
+                //ajax request to add this like, if the user hasn't liked it before
+            });
+
+            $(".readLater").click(function(e){
+            // e.preventDefault();
+                alert("add ability to read later");
+                //ajax request to add this like, if the user hasn't liked it before
+            });
+             
+        },
+        loadArticleData : function(){
+ 
+            $.ajax({
+                url:"/json/articles.json",
+                method:'get',
+                success:this.handlerData
+ 
+            })
+        }
+};
+ 
+$(document).ready(function(){
+ 
+    ARTICLE_METHOD.loadArticleData();
+});
+
