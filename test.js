8feb95ffc37f88b7decd413b9cfa410fc87f7617
nlj24 <nlj24@cornell.edu>
2014-04-22 19:26:49 -0400

    oy

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var app = express();
  /* serves all the static files */
  app.get('/', function(req, res){ 
      console.log('static file request : ' + req.params);
-     res.sendfile("./test.html"); 
+     res.sendfile("test.html"); 
  });
  
  var port = process.env.PORT || 5000;
