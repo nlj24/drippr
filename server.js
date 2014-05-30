d16bdb9cd16cd0e63770a30e6e37e0bb66d827a7
Ashwin Raja <ashwin.craja@gmail.com>
2014-05-30 15:26:27 -0400

    jjj

--- server.js
+++ server.js
@@ -142,7 +142,8 @@ app.get("/is_user",  function(req, res){
 app.post("/shadow_users",  function(req, res){
     req.setEncoding('utf8');
     req.on('data', function(chunk) {
-        // var data = JSON.parse(chunk);
+        var data = JSON.parse(chunk);
+        data = chunk;
         console.log(data);
         var friend_id_lst = data.lst;
         var friend_id_string = friend_id_lst.join();
