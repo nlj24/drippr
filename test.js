4c74a12a206f45a8721c981c2909c157ad7d0382
Ashwin Raja <ashwin.craja@gmail.com>
2014-05-30 14:55:11 -0400

    some testing

--- test.js
+++ test.js
@@ -5,10 +5,24 @@ var app = express();
  /* serves all the static files */
  app.get('/', function(req, res){ 
      console.log('static file request : ' + req.params);
-     res.sendfile(__dirname + "/index.html"); 
+     res.sendfile(__dirname + "/test.html"); 
+ });
+
+ app.get(/^(.+)$/, function(req, res){ 
+    res.sendfile( __dirname + req.params[0]); 
+});
+
+ app.post('/postTest', function(req, res){
+	req.setEncoding('utf8');
+    var body;
+    req.on('data', function(chunk) {
+        body = chunk;
+        console.log(body);
+    });
+ 	res.send("hey");
  });
  
- var port = process.env.PORT || 5000;
+ var port = process.env.PORT || 5003;
  
  app.listen(port, function() {
    console.log("Listening on " + port);
