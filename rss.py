6a52ea04025e2d2f329a8d859e0512306530039b
dp392@cornell.edu <dp392@cornell.edu>
2014-06-01 17:49:15 -0400

    dfs

--- rss.py
+++ rss.py
@@ -1,10 +1,11 @@
 import feedparser, urllib2
 import MySQLdb
 from random import shuffle
+from time import mktime
+from datetime import datetime
 
 db = MySQLdb.connect('54.86.82.21','root','drippr','drippr_db')
 cursor = db.cursor()
-# rss = [("ESPN","http://sports.espn.go.com/espn/rss/news"),("Fox News", "http://feeds.foxnews.com/foxnews/latest"),("CNN", "http://rss.cnn.com/rss/cnn_topstories.rss")]
 articlesToAdd = []
 
 try:
@@ -23,19 +24,23 @@ try:
 	for url in articleUrls:
 		articleUrls2.append(url[0])
 
+	dateListFail = []
+	b = 0
 	for (id, source, url, category) in rss2:
 		feed = feedparser.parse(url)
 		lst = feed['items']
 		for a in lst:
 			print 1
+			b = b + 1
 			headline_result = a['title'].encode('utf-8')
 			url_result = a['link']
 			try:
-				print source
-				date=a['published_parsed']
-				# date = a['published']
+				date = a['published_parsed']
+				date = str(datetime.fromtimestamp(mktime(date)))
 			except:
 				date = 'no date'
+				dateListFail.append(source)
+				print date
 			request = urllib2.Request(url_result)
 			request.add_header("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; es-ES; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5")
 			try:
@@ -50,14 +55,13 @@ try:
 			if (url_result) not in articleUrls2:
 				print url_result
 				articlesToAdd.append({'headline':headline_result,'image':image_result,'url':url_result,'source':source,'date': date, 'category':category})
-			else:
-				print 2
-
+	print dateListFail
 	shuffle(articlesToAdd)
+	print articlesToAdd
 	for article in articlesToAdd:
 		try:
 			# execute SQL query using execute() method.
-			sql = "INSERT INTO Articles (headline, imgUrl, url, source, date, category, numLikes, numDislikes, collected) VALUES (\'" + article['headline'] + "\',\'" + article['image'] + "\',\'" + article['url'] + "\',\'" + article['source'] + "\',\'" + article['date'] + "\',\'"  + article['category'] + "\',0,0,1)"
+			sql = "INSERT INTO Articles (headline, imgUrl, url, source, date, category, numLikes, numDislikes, collected) VALUES (\"" + article['headline'] + "\",\'" + article['image'] + "\',\'" + article['url'] + "\',\'" + article['source'] + "\',\'" + article['date'] + "\',\'"  + article['category'] + "\',0,0,1)"
 			print sql
 			cursor.execute(sql)
 			db.commit()
@@ -65,6 +69,7 @@ try:
 			db.rollback()
 
 	db.close()
+	print b
 except Exception as e:
 	print e
 	db.rollback()
