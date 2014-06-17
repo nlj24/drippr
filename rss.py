7549d8870f4c5b6167079e45706732e366e5f11e
Darshan Patel <dp392@cornell.edu>
2014-06-16 23:41:36 -0400

    fdsfs

--- rss.py
+++ rss.py
@@ -11,7 +11,7 @@ articlesToAdd = []
 print "starting script"
 try:
 	# execute SQL query using execute() method.
-	sql = "SELECT * FROM Sources where id = 5"
+	sql = "SELECT * FROM Sources where id = 53"
 	cursor.execute(sql)
 	rss = cursor.fetchall()
 	rss2 = []
