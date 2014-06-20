57c2504898b2fef95dcf552ddab535c98507fecf
Darshan Patel <dp392@cornell.edu>
2014-06-20 10:39:59 -0400

    fdsfs

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -479,6 +479,7 @@ $(document).keydown(function(e){
 										$(".addMemsNew").unbind("click", handler4);
 										var handler4 = $(".addMemsNew").click(function(e){
 								            $("#addChosenCont").html('');
+								            $("#chosenAdd").html('');
 								            window.addIds = [];
 								            window.addChosenFriends = {};
 								            $(".showForm").attr("class", "showForm");
