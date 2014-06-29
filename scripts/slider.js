788b7126f19a8e9fbf27358c245db783a75b58fd
Darshan Patel <dp392@cornell.edu>
2014-06-29 15:59:39 -0400

    fdsfsd

--- scripts/slider.js
+++ scripts/slider.js
@@ -42,7 +42,7 @@ var Slider = function() { this.initialize.apply(this, arguments) }
     	window.positions[window.curCategory] = index;
     	console.log(window.positions);
     	console.log(window.curArticlePosition);
-    	window.curArticlePosition[window.curCategory][window.curArticle] = window.positions[window.curCategory];
+    	// window.curArticlePosition[window.curCategory][window.curArticle] = window.positions[window.curCategory];
 
     	window.setDrippLikes();
         
