845b75f37dbe675763e0a2900c8ae37e486bda8d
nlj24 <nlj24@cornell.edu>
2014-07-12 13:29:09 -0400

    ok

---
+++ scripts/unsubscribe.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+function getParameterByName( name,href ){
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( href );
+    if( results == null )
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+console.log("id", window.location.href);
