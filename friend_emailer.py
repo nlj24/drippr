3ac3d2ce8cf2e1380667a89433605bf0d5c7f4e4
Ashwin Raja <ashwin.craja@gmail.com>
2014-07-22 00:24:00 -0700

    some emailing thigns

--- friend_emailer.py
+++ friend_emailer.py
@@ -14,14 +14,14 @@ graph = facebook.GraphAPI(access_token)
 
 
 try:
-    # sql = "SELECT fName, lName, Users.id, email, emailable, FROM Users"
-    # cursor.execute(sql)
-    # all_users = cursor.fetchall()
-    # all_users_dict = {}
-    # for user in all_users:
-    # 	all_users_dict[user[2]] = user
+    sql = "SELECT fName, lName, Users.id, email, emailable, FROM Users"
+    cursor.execute(sql)
+    all_users = cursor.fetchall()
+    all_users_dict = {}
+    for user in all_users:
+    	all_users_dict[user[2]] = user
 
-    # print all_users_dict
+    print all_users_dict
 
     sql = "SELECT fName, Users.id, lName FROM Users WHERE created>NOW() - INTERVAL 3 DAY"
     # sql = "SELECT email, fName FROM Dripps WHERE unreadDripps=1 INNER JOIN Users ON Dripps.recipientId=Users.id GROUP BY Dripps.recipientId"
@@ -36,8 +36,25 @@ except Exception as e:
     db.rollback()
     db.close()
 
+email_dict = {}
 for user in new_users:
 	#get friend list from fb
 	print user
 	friends = graph.get_connections(user[1], 'friends')
-	print friends
+
+	while True:
+	    try:
+	        # Perform some action on each post in the collection we receive from
+	        # Facebook.
+	        for friend in friends:
+	        	if friend['id'] in all_users_dict:
+	        		if friend['id'] not in email_dict:
+	        			email_dict[friend['id']] = []
+	        		email_dict[friend['id']].push(user)
+	        # Attempt to make a request to the next page of data, if it exists.
+	        friends = requests.get(friends['paging']['next']).json()
+	    except KeyError:
+	        # When there are no more pages (['paging']['next']), break from the
+	        # loop and end the script.
+	        break
+print email_dict
