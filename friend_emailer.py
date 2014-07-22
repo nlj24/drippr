19dbb88faef72e4b9306f47d074df7391b8509ec
Ashwin Raja <ashwin.craja@gmail.com>
2014-07-22 00:46:50 -0700

    some emailing thigns

--- friend_emailer.py
+++ friend_emailer.py
@@ -36,7 +36,6 @@ try:
 				if friend['id'] not in candidate_emails:
 					candidate_emails[friend['id']] = []
 				candidate_emails[friend['id']].append(user)
-
 	for user in candidate_emails:
 		if all_users_dict[user][4] = '1':
 			to_send.append({"fName": all_users_dict[user][0] , "lName": all_users_dict[user][1] , "email": all_users_dict[user][3] , "friends_joined": candidate_emails[user]})
