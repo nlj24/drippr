ba0653a48a3fcf9f438394785accaed5c1c18e5e
Ashwin Raja <ashwin.craja@gmail.com>
2014-07-21 23:50:16 -0700

    some emailing thigns

--- friend_emailer.py
+++ friend_emailer.py
@@ -8,11 +8,11 @@ db = MySQLdb.connect('54.86.82.21','root','drippr','drippr_db')
 cursor = db.cursor()
 
 try:
-    sql = "SELECT fName, email, Users.id, lName FROM Users WHERE unreadDripps=1 AND emailable=1 AND isReal=1 GROUP BY Dripps.recipientUserId"
+    sql = "SELECT fName, email, Users.id, lName FROM Users WHERE created>NOW() - INTERVAL 3 DAY"
     # sql = "SELECT email, fName FROM Dripps WHERE unreadDripps=1 INNER JOIN Users ON Dripps.recipientId=Users.id GROUP BY Dripps.recipientId"
     cursor.execute(sql)
-    email_data = cursor.fetchall()
-    print email_data
+    new_users = cursor.fetchall()
+    print new_users
 except Exception as e:
     print e
     db.rollback()
